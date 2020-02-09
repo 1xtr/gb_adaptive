ceef5335b82be6a3146307b8795b65f26d7e8163
1XTR.homepc <1XTR.homepc@trendco.space>
2020-02-09 15:42:35 +0300

    Changes 02/09/20 всплывающее меню, эффект blur для всего остального

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function blur_body() {
-    let blur = document.getElementById('blur').className;
-    if (blur == "blur_active") {
-        blur = "blur";
-    } else {
-        blur += "_active";
+    // let blur = document.getElementById('blur');
+    // if (blur.className == "blur" ) {
+    //     blur ;
+    // } else {
+    //     blur += "_active";
+    // }
+    let blur = document.getElementById("blur");
+    let navRight = document.getElementById("nav").style.right;
+    if (navRight === "" || navRight === "-100%") {
+        document.getElementById("nav").style.right = 0;
+        blur.classList.add("blur_active");
+    } else if (navRight === "0px") {
+        document.getElementById("nav").style.right = "-100%";
+        blur.classList.remove("blur_active");
     }
-    console.log(blur);
+    console.log(navRight);
+
 }
