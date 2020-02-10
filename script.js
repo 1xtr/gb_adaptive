cf854f164fd2da3eba801e9b36c55a57e1d741f5
1XTR.homepc <1XTR.homepc@trendco.space>
2020-02-10 09:05:24 +0300

    Changes 02/10/20

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function blur_body() {
     let navRight = document.getElementById("nav").style.right;
     if (navRight === "" || navRight === "-100%") {
         document.getElementById("nav").style.right = 0;
-        //document.getElementById("nav").style.display = "block";
+        document.getElementById("nav").style.display = "block";
         blur.classList.add("blur_active");
     } else if (navRight === "0px") {
         document.getElementById("nav").style.right = "-100%";
-        //document.getElementById("nav").style.display = "none";
+        document.getElementById("nav").style.display = "none";
         blur.classList.remove("blur_active");
     }
     //console.log(navRight);
